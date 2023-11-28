a28e456ae12f92a7d90b961388ad026524c6fd46
Ebrahim Beiati-Asl <ebrahimbeiaty@gmail.com>
2023-11-28 22:21:30 +0000

    new edit

--- client1/src/firebase.js
+++ client1/src/firebase.js
@@ -1,12 +1,5 @@
-
-
-
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "real-estate-app-7da91.firebaseapp.com",
